45fd72bc7d2b524755949dfe6b4832b974c092ad
Pum <28806828+PumPum7@users.noreply.github.com>
2024-10-30 22:17:17 +0100

    add deserialize and serialize to Value

--- src/storage/value.rs
+++ src/storage/value.rs
@@ -1,4 +1,6 @@
-#[derive(Debug, Clone, PartialEq)]
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub enum Value {
     Integer(i64),
     Float(f64),
